7ca429b1565137867963d8af854f1b0f8fb9ed10
Zethtar <Anto_999@hotmail.ca>
2016-11-28 09:38:36 -0500

    Changed whole file

---
+++ TP3Etu/TP3/Jeu_Main.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace TP3
+{
+  partial class frm_Jeu_Main
+  {
+    /// <summary>
+    /// Variable nécessaire au concepteur.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Nettoyage des ressources utilisées.
+    /// </summary>
+    /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+    protected override void Dispose( bool disposing )
+    {
+      if ( disposing && ( components != null ) )
+      {
+        components.Dispose( );
+      }
+      base.Dispose( disposing );
+    }
+
+    #region Code généré par le Concepteur Windows Form
+
+    /// <summary>
+    /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+    /// le contenu de cette méthode avec l'éditeur de code.
+    /// </summary>
+    private void InitializeComponent( )
+    {
+      this.tableauDeJeu = new System.Windows.Forms.TableLayoutPanel();
+      this.SuspendLayout();
+      // 
+      // tableauDeJeu
+      // 
+      this.tableauDeJeu.ColumnCount = 20;
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.63538F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 7.220217F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.01805F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+      this.tableauDeJeu.Location = new System.Drawing.Point(173, 42);
+      this.tableauDeJeu.Margin = new System.Windows.Forms.Padding(0);
+      this.tableauDeJeu.Name = "tableauDeJeu";
+      this.tableauDeJeu.RowCount = 30;
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 3.628941F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6.350646F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.001134F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+      this.tableauDeJeu.Size = new System.Drawing.Size(222, 441);
+      this.tableauDeJeu.TabIndex = 1;
+      // 
+      // frm_Jeu_Main
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(578, 589);
+      this.Controls.Add(this.tableauDeJeu);
+      this.KeyPreview = true;
+      this.Name = "frm_Jeu_Main";
+      this.Text = "Tetris";
+      this.Load += new System.EventHandler(this.frmLoad);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TableLayoutPanel tableauDeJeu;
+  }
+}
+
---
+++ TP3Etu/TP3/Jeu_Main.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TP3
+{
+  public partial class frm_Jeu_Main : Form
+  {
+    public frm_Jeu_Main( )
+    {
+      InitializeComponent( );
+    }
+
+    #region Code fourni
+    
+    // Représentation visuelles du jeu en mémoire.
+    PictureBox[,] toutesImagesVisuelles = null;
+    
+    /// <summary>
+    /// Gestionnaire de l'événement se produisant lors du premier affichage 
+    /// du formulaire principal.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void frmLoad( object sender, EventArgs e )
+    {
+      // Ne pas oublier de mettre en place les valeurs nécessaires à une partie.
+      ExecuterTestsUnitaires();
+      InitialiserSurfaceDeJeu(20,10);
+    }
+
+    private void InitialiserSurfaceDeJeu(int nbLignes, int nbCols)
+    {
+      // Création d'une surface de jeu 10 colonnes x 20 lignes
+      toutesImagesVisuelles = new PictureBox[nbLignes, nbCols];
+      tableauDeJeu.Controls.Clear();
+      tableauDeJeu.ColumnCount = toutesImagesVisuelles.GetLength(1);
+      tableauDeJeu.RowCount = toutesImagesVisuelles.GetLength(0);
+      for (int i = 0; i < tableauDeJeu.RowCount; i++)
+      {
+        tableauDeJeu.RowStyles[i].Height = tableauDeJeu.Height / tableauDeJeu.RowCount;
+        for (int j = 0; j < tableauDeJeu.ColumnCount; j++)
+        {
+          tableauDeJeu.ColumnStyles[j].Width = tableauDeJeu.Width / tableauDeJeu.ColumnCount;
+          // Création dynamique des PictureBox qui contiendront les pièces de jeu
+          PictureBox newPictureBox = new PictureBox();
+          newPictureBox.Width = tableauDeJeu.Width / tableauDeJeu.ColumnCount;
+          newPictureBox.Height = tableauDeJeu.Height / tableauDeJeu.RowCount;
+          newPictureBox.BackColor = Color.Black;
+          newPictureBox.Margin = new Padding(0, 0, 0, 0);
+          newPictureBox.BorderStyle = BorderStyle.FixedSingle;
+          newPictureBox.Dock = DockStyle.Fill;
+
+          // Assignation de la représentation visuelle.
+          toutesImagesVisuelles[i, j] = newPictureBox;
+          // Ajout dynamique du PictureBox créé dans la grille de mise en forme.
+          // A noter que l' "origine" du repère dans le tableau est en haut à gauche.
+          tableauDeJeu.Controls.Add(newPictureBox, j, i);
+        }
+      }
+    }
+    #endregion
+
+
+
+
+
+    #region Code à développer
+    /// <summary>
+    /// Faites ici les appels requis pour vos tests unitaires.
+    /// </summary>
+    void ExecuterTestsUnitaires()
+    {      
+      ExecuterTestABC();
+      // A compléter...
+    }
+
+    // A renommer et commenter!
+    void ExecuterTestABC()
+    {
+      // Mise en place des données du test
+      
+      // Exécuter de la méthode à tester
+      
+      // Validation des résultats
+      
+      // Clean-up
+    }
+
+    #endregion
+
+  }
+
+
+
+
+
+
+
+
+}
